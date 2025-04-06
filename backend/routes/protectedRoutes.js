3507771e6778948a08cf242ed756acd4d37d90a9
Divyanshu8957 <divyanshu.ai8957@gmail.com>
2025-04-06 21:26:57 +0530

    fix: authMiddleware file path error

--- backend/routes/protectedRoutes.js
+++ backend/routes/protectedRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require('../Middleware/authMiddleware');
 
 const router = express.Router();
 
