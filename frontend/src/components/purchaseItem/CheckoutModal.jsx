62246489397fec8016764d01f6de7fc764ddcec2
Mohammad Umar <mohammadumar8010@gmail.com>
2025-04-08 15:31:24 +0530

    payment method error

--- frontend/src/components/purchaseItem/CheckoutModal.jsx
+++ frontend/src/components/purchaseItem/CheckoutModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import PaymentMethod from "./PaymentMethod"; // Import Payment Modal
+import PaymentMethod from "./Paymentmethod"; // Import Payment Modal
 const CheckoutModal = ({ isOpen, onClose, cartItems, totalPrice, userId }) => {
   if (!isOpen) return null;
 
